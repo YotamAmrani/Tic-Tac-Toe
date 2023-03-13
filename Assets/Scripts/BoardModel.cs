cb9fac383ba3a20b402d6efd47c6741634924e6f
yoyo <yotam-ra.amrani@mail.huji.com>
2023-03-13 20:28:43 +0200

    adding boardModel

---
+++ Assets/Scripts/BoardModel.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoardModal : MonoBehaviour
+{
+    private const int BOARD_SIZE = 3;
+    private const int PLAYERS_COUNT = 2;
+    private int[,] board = new int[BOARD_SIZE, BOARD_SIZE];
+    private Player[] players = new Player[PLAYERS_COUNT];
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
