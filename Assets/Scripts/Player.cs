ce22ab321c0408f0730658beeeda8c32bfc8f4ff
yoyo <yotam-ra.amrani@mail.huji.com>
2023-04-10 20:50:25 +0300

    changed player to be non-mono

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,8 +1,15 @@
 using UnityEngine;
-public class Player : MonoBehaviour
+public class Player
 {
     public BoardModel.Mark playerSign;
     public Sprite playerSprite;
     public string playerName;
+
+    public Player(BoardModel.Mark pSign, Sprite pSprite, string pName)
+    {
+        playerSign = pSign;
+        playerName = pName;
+        playerSprite = pSprite;
+    }
 
 }
